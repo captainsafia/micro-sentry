59c4722a28eea53ff2cb1215dcc70e77bbe01760
Safia Abdalla <safia@safia.rocks>
2019-11-23 17:44:13 -0800

    Update @hapi/boom dependency

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-const { send } = require('micro');
-const Boom = require('boom');
-const Raven = require('raven');
+const { send } = require("micro");
+const Boom = require("@hapi/boom");
+const Raven = require("raven");
 
 module.exports = exports = url => fn => {
-  if (!url)
-    throw Error('micro-sentry must be initialized with a Sentry DSN.');
+  if (!url) throw Error("micro-sentry must be initialized with a Sentry DSN.");
 
-  if (!fn || typeof fn !== 'function')
-    throw Error('micro-sentry must be passed a function.');
+  if (!fn || typeof fn !== "function")
+    throw Error("micro-sentry must be passed a function.");
 
   Raven.config(url).install();
   return async function(request, response) {
@@ -17,9 +16,12 @@ module.exports = exports = url => fn => {
       Raven.captureException(error);
       let status = response.statusCode;
       if (status < 400) status = 500;
-      const err = Boom.wrap(error, status);
-      send(response, status, Object.assign({}, err.output.payload, 
-        err.data && { data : err.data }));
+      const err = Boom.boomify(error, { statusCode: status });
+      send(
+        response,
+        status,
+        Object.assign({}, err.output.payload, err.data && { data: err.data })
+      );
     }
-  }
+  };
 };
